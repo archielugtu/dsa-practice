431498f665b04974ff642cee401db6ed2e73538a
Rchi Lugtu <archeeeese.dev@gmail.com>
2025-05-22 23:54:32 +1200

    Solve IsAnagram

--- DSACsharp/ArraysAndHashing.cs
+++ DSACsharp/ArraysAndHashing.cs
@@ -18,4 +18,35 @@ public static class ArraysAndHashing
 
     return [0, 0];
   }
+
+  public static bool IsAnagram(string s, string t)
+  {
+    // if (s.Length != t.Length) return false;
+
+    // var seen = new Dictionary<char, int>();
+    // foreach (char c in s)
+    // {
+    //   seen[c] = seen.GetValueOrDefault(c, 0) + 1;
+    // }
+
+    // foreach (char c in t)
+    // {
+    //   if (!seen.TryGetValue(c, out int value) || value == 0) return false;
+    //   seen[c] = seen[c] - 1;
+    // }
+
+    // return true;
+
+    if (s.Length != t.Length) return false;
+
+    Dictionary<char, int> countS = new Dictionary<char, int>();
+    Dictionary<char, int> countT = new Dictionary<char, int>();
+
+    for (int i = 0; i < s.Length; i++) {
+        countS[s[i]] = countS.GetValueOrDefault(s[i], 0) + 1;
+        countT[t[i]] = countT.GetValueOrDefault(t[i], 0) + 1;
+    }
+
+    return countS.Count == countT.Count && !countS.Except(countT).Any();
+  }
 }
--- DSACsharp/Program.cs
+++ DSACsharp/Program.cs
@@ -1,3 +1,5 @@
 ﻿using DSACsharp;
 
-Console.WriteLine(string.Join(", ", ArraysAndHashing.TwoSum([3, 6, 1, 8], 11)));
+// Console.WriteLine(string.Join(", ", ArraysAndHashing.TwoSum([3, 6, 1, 8], 11)));
+Console.WriteLine(ArraysAndHashing.IsAnagram("anagram", "nagagram"));
+
